1084b650a7a6675360b2d1c1bae25ce63300c1b9
liuxin <liuxin@boluome.com>
2018-03-15 22:03:34 +0800

    config-client 配置文件

--- Spring-learning-config-client/src/main/java/smile/config/client/ConfigClientApplication.java
+++ Spring-learning-config-client/src/main/java/smile/config/client/ConfigClientApplication.java
@@ -3,6 +3,8 @@ package smile.config.client;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.autoconfigure.ConfigurationPropertiesRebinderAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,8 +23,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class ConfigClientApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConfigClientApplication.class, args);
-
-
     }
     @Value("${foo}")
     String foo;
