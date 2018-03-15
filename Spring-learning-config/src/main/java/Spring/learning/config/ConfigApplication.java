0bbf96dfd8ad077ede529b22d5c4eed129c511ec
liuxin <liuxin@boluome.com>
2018-03-15 17:47:58 +0800

    config-client 配置文件

--- Spring-learning-config/src/main/java/Spring/learning/config/ConfigApplication.java
+++ Spring-learning-config/src/main/java/Spring/learning/config/ConfigApplication.java
@@ -2,7 +2,11 @@ package Spring.learning.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableConfigServer
+@EnableEurekaClient
 @SpringBootApplication
 public class ConfigApplication {
 
